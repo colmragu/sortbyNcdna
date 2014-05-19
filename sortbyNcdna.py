397b6b4e942a8ab31b5e9180437a4802b0d5423a
Colm Herbert <colmherbert@gmail.com>
2014-05-19 16:30:43 +0100

    replaced path to file with filename in print out

--- sortbyNcdna.py
+++ sortbyNcdna.py
@@ -1,3 +1,19 @@
+#Copyright 2014 Colm Herbert 
+
+
+#This program is free software: you can redistribute it and/or modify
+#it under the terms of the GNU General Public License as published by
+#the Free Software Foundation, either version 3 of the License, or
+#(at your option) any later version.
+
+#This program is distributed in the hope that it will be useful,
+#but WITHOUT ANY WARRANTY; without even the implied warranty of
+#MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#GNU General Public License for more details.
+
+#You should have received a copy of the GNU General Public License
+#along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 from __future__ import print_function
 from Bio import SeqIO
 import pdb
@@ -16,26 +32,29 @@ def sortbyNcdna(sortbyNCDNA, gb_file):
     return (sortbyNCDNA) 
   f.close()
   for feature in gb_record.features[1:]: #Select all but the first entry in gbk file
+
     feature = pad_gb_features(feature)
     ncdna = str(gb_record.seq[scan_pos:feature.location.start.position+1]) #ncdna = non coding dna
     if ncdna == "":
-      sortbyNCDNA = addfeaturetodict(ncdna_last,sortbyNCDNA, feature, gb_file)
+      sortbyNCDNA = addfeaturetodict(ncdna_last,sortbyNCDNA, feature, gb_record.name)
     else:
-      sortbyNCDNA = addfeaturetodict(ncdna,sortbyNCDNA, feature, gb_file)
+      sortbyNCDNA = addfeaturetodict(ncdna,sortbyNCDNA, feature, gb_record.name)
       ncdna_last= ncdna
     scan_pos = feature.location.end.position
   return(sortbyNCDNA)
 
-def addfeaturetodict(ncdna,sortbyNCDNA, feature, organism):
+def addfeaturetodict(ncdna,sortbyNCDNA, feature, locus):
   if not sortbyNCDNA.has_key(ncdna):
     sortbyNCDNA[ncdna]={}
   if not sortbyNCDNA[ncdna].has_key(feature.qualifiers["db_xref"][0]):
     sortbyNCDNA[ncdna][feature.qualifiers["db_xref"][0]] ={} 
-
-  #sortbyNCDNA[ncdna][feature.qualifiers["db_xref"][0]] = feature.qualifiers["note"].join(feature.location.start).join(feature.location.end)
-  sortbyNCDNA[ncdna][feature.qualifiers["db_xref"][0]] = feature.qualifiers["note"]  ,int (feature.location.start) ,int (feature.location.end) ,organism #add more information
-
+  sortbyNCDNA[ncdna][feature.qualifiers["db_xref"][0]] =colourstring(feature.qualifiers["note"]  ,int (feature.location.start) ,int (feature.location.end) ,locus) #add more information
   return(sortbyNCDNA) 
+def colourstring(note, start, end , locus):
+
+  colouredString = note ,start ,end, locus
+
+  return(colouredString)
 
 def pad_gb_features(feature):
   ### Make sure all gb objects conatain a dic entry for note, db_xref
